89b25c87336a053023831b56afe73645635c0a83
Bear Nithi <bearnithi@gmail.com>
2018-08-25 17:57:14 +0530

    First Commit

---
+++ src/lib/bn-idle.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BnIdleService } from './bn-idle.service';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [],
+  exports: [BnIdleService]
+})
+export class BnIdleModule { }
---
+++ src/public_api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of bn-idle
+ */
+
+export * from './lib/bn-idle.service';
+export * from './lib/bn-idle.module';
