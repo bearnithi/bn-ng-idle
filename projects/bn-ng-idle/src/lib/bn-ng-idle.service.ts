c76e967aaef210dc6538654b49328436f68ea9cb
Nithi <58935041+imNithi@users.noreply.github.com>
2023-01-30 17:00:36 +0530

    reset timer added

--- projects/bn-ng-idle/src/lib/bn-ng-idle.service.ts
+++ projects/bn-ng-idle/src/lib/bn-ng-idle.service.ts
@@ -1,23 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Observable, fromEvent, merge, Subject, timer, Subscription } from 'rxjs';
-
-
+​
+​
 @Injectable({
   providedIn: 'root'
 })
 export class BnNgIdleService {
-
-  private idle$: Observable<any>;
-  private timer$: Subscription;
-  private timeOutMilliSeconds: number;
-  private idleSubscription: Subscription;
-
+​
+  private idle$!: Observable<any>;
+  private timer$!: Subscription;
+  private timeOutMilliSeconds!: number;
+  private idleSubscription!: Subscription;
+​
   public expired$: Subject<boolean> = new Subject<boolean>();
-
+​
   constructor() {
-
+​
   }
-
+​
   public startWatching(timeOutSeconds: number): Observable<any> {
     this.idle$ = merge(
       fromEvent(document, 'mousemove'),
@@ -31,29 +31,32 @@ export class BnNgIdleService {
       fromEvent(window, 'mousemove'),
       fromEvent(window, 'resize'),
     );
-
+​
     this.timeOutMilliSeconds = timeOutSeconds * 1000;
-
+​
     this.idleSubscription = this.idle$.subscribe((res) => {
       this.resetTimer();
     });
-
+​
     this.startTimer();
-
+​
     return this.expired$;
   }
-
+​
   private startTimer() {
     this.timer$ = timer(this.timeOutMilliSeconds, this.timeOutMilliSeconds).subscribe((res) => {
       this.expired$.next(true);
     });
   }
-
-  public resetTimer() {
+​
+  public resetTimer(timeOutSeconds?: number) {
     this.timer$.unsubscribe();
+    if (timeOutSeconds) {
+      this.timeOutMilliSeconds = timeOutSeconds * 1000;
+    }
     this.startTimer();
   }
-
+​
   public stopTimer() {
     this.timer$.unsubscribe();
     this.idleSubscription.unsubscribe();
